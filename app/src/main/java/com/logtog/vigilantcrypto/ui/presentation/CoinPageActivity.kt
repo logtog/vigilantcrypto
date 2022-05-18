a67fcb8f9101aeec3271706a2b3bfe3d91884e20
logtog <thiagogamexp@outlook.com>
2022-05-17 22:40:22 -0300

    implemented page activity for coins

---
+++ app/src/main/java/com/logtog/vigilantcrypto/ui/presentation/CoinPageActivity.kt
@@ -0,0 +1,12 @@
+package com.logtog.vigilantcrypto.ui.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.logtog.vigilantcrypto.R
+
+class CoinPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coin_page)
+    }
+}
